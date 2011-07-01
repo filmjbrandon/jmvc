962585c2c0f36507b50358f4df655469a60a3a7b
Don Myers <donmyers@mac.com>
2011-07-01 09:45:36 -0400

    removed some old files

--- application/controllers/old/form_test_fail.php
+++
@@ -1,7 +0,0 @@
-<?php
-$output['mvc_model_valid'] = false;
-
-header('Content-type: text/json');
-header('Content-type: application/json');
-
-die(json_encode($output));
--- application/controllers/old/form_test_pass.php
+++
@@ -1,7 +0,0 @@
-<?php
-$output['mvc_model_valid'] = true;
-
-header('Content-type: text/json');
-header('Content-type: application/json');
-
-die(json_encode($output));
