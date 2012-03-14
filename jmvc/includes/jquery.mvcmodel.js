50f62dcbfb5d6e149e8b0ca9811ea8bdfe3c4d5f
Don Myers <donmyers@mac.com>
2012-03-14 08:19:08 -0400

    made primary not need to be "id"

--- jmvc/includes/jquery.mvcmodel.js
+++ jmvc/includes/jquery.mvcmodel.js
@@ -1,7 +1,11 @@
-/* I attach all models to the mvc super global mvc.user_m = new mvcModel('user'); */
-function mvcModel(name){
+/*
+I attach all models to the mvc super global mvc.user_m = new mvcModel('user');
+They talk to the backend via a REST Server
+*/
+mvcModel = function(name,primary){
 	this._clear();
 	this._table = name;
+	this._primary = (primary) ? primary : 'id';
 }
 
 mvcModel.prototype._clear = function() {
@@ -11,19 +15,19 @@ mvcModel.prototype._clear = function() {
   this._error = '';
   this._error_no = 0;
   this._internal_id = this._id();
-	this.id = -1;
+	this._primary = '';
 }
 
-/* select */
+/* select / get */
 mvcModel.prototype._get = function(url) {
 	url = (url) || '';
-	url = (this.id != -1) ? '/' + this.id : url;
+	url = (this.[this._primary] != -1) ? '/' + this[this._primary] : url;
 	this.__ajax(mvc.rest_url + this._table + url,'get',{});
 }
 
 /* insert unless there is a id then update */
 mvcModel.prototype._post = function(method) {
-	arg = (this.id != -1) ? '/' + this.id : '';
+	arg = (this[this._primary] != -1) ? '/' + this[this._primary] : '';
 	arg = (method) ? arg : '/' + method;
 	this.__ajax(mvc.rest_url + this._table + arg,'post',mvc.clone(this));
 }
@@ -31,21 +35,17 @@ mvcModel.prototype._post = function(method) {
 /* update */
 mvcModel.prototype._put = function(method) {
 	method = (method) ? '/' + method : '';
-	this.__ajax(mvc.rest_url + this._table + method,'post',mvc.clone(this));
+	this.__ajax(mvc.rest_url + this._table + method,'put',mvc.clone(this));
 }
 
 /* delete */
 mvcModel.prototype._delete = function(method) {
-	arg = (this.id != -1) ? '/' + this.id : '';
+	arg = (this[this._primary] != -1) ? '/' + this[this._primary] : '';
 	arg = (method) ? arg : '/' + method;
 	this.__ajax(mvc.rest_url + this._table + arg,'delete',mvc.clone(this));
 }
 
-/* generic REST */
-mvcModel.prototype._sync = function(extras) {
-	this.__ajax(mvc.model_url + this._table + '.js', 'post', {table: this._table, record: mvc.clone(this), extra: extra});
-};
-
+/* used by all the REST functions */
 mvcModel.prototype.__ajax = function(url,method,data) {
   /* send it out via blocking ajax */
 	var json = jQuery.mvcAjax({url: url, data: data, method: method}) || {};
@@ -56,6 +56,7 @@ mvcModel.prototype.__ajax = function(url,method,data) {
   console.log(this);
 }
 
+/* fetch the current db record and move the cursor forward 1 - returns false if at last record */
 mvcModel.prototype._fetch = function() {
   if ((this._index + 1) > this._rows_affected) {
     return false;
@@ -64,6 +65,7 @@ mvcModel.prototype._fetch = function() {
   return this._records[this._index];
 };
 
+/* move the database cursor to a specific record */
 mvcModel.prototype._seek = function(index) {
   if (index > this._rows_affected || index < 0) {
     return false;
@@ -72,6 +74,7 @@ mvcModel.prototype._seek = function(index) {
   return true;
 };
 
+/* create a unique id */
 mvcModel.prototype._id = function () {
   return "xxxxxxxxxxxxxxxyxxxxxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c === "x" ? r : (r & 0x3 | 0x8);
